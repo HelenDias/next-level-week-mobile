5544a0b5ef9bd57534406ad8410d5dd64f23c288
Helen Dias <helendias.hd@gmail.com>
2020-06-08 03:39:32 -0300

    :art: Add function to navigate back

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -6,11 +6,18 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 const Points = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleNavigateBack}>
         <Icon name="arrow-left" size={20} color="#34cb79" />
       </TouchableOpacity>
     </View>
