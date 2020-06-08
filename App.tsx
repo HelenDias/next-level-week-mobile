4a633d7616ba63afd3a6f4dcaf9b07132a4351e5
Helen Dias <helendias.hd@gmail.com>
2020-06-08 02:19:24 -0300

    :sparkles: Add StatusBar in initial page

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { StatusBar, View } from 'react-native';
 import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
   );
 }
