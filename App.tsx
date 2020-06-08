d9d316bc83a38b10db6afcffb304372eeaa55c66
Helen Dias <helendias.hd@gmail.com>
2020-06-08 02:10:03 -0300

    :fire: Clear home file

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
