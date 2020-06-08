2223decd20c09557e2e17409928974d630fe380e
Helen Dias <helendias.hd@gmail.com>
2020-06-08 03:02:43 -0300

    :lipstick: Finish homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,8 @@ import {
   StyleSheet,
   ImageBackground,
 } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Home = () => {
   return (
@@ -16,12 +18,27 @@ const Home = () => {
     >
       <View style={styles.main}>
         <Image source={require('../../assets/logo.png')} />
+
         <Text style={styles.title}>
           Seu marketplace de coleta de resíduos
         </Text>
+
         <Text style={styles.description}>
           Ajudamos pessoas a encontrar pontos de coleta de forma eficiente
         </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
       </View>
     </ImageBackground>
   )
